bfc43d222c0af8904b75977926a69f1fbe901a38
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-09-09 22:38:02 +0700

    Add RecyclerView

--- app/src/main/java/com/ahmadabuhasan/appgithubuser/MainActivity.java
+++ app/src/main/java/com/ahmadabuhasan/appgithubuser/MainActivity.java
@@ -1,14 +1,25 @@
 package com.ahmadabuhasan.appgithubuser;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView_User;
+    ArrayList<User> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView_User.setLayoutManager(new LinearLayoutManager(this));
+        UserAdapter userAdapter = new UserAdapter(list);
+        recyclerView_User.setAdapter(userAdapter);
     }
 }
