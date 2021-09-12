b6268b77d41bd60fafc1821fc667af40d8586f96
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-09-13 05:18:21 +0700

    Update JAVA UserAdapter.java

--- app/src/main/java/com/ahmadabuhasan/appgithubuser/UserAdapter.java
+++ app/src/main/java/com/ahmadabuhasan/appgithubuser/UserAdapter.java
@@ -3,9 +3,14 @@ package com.ahmadabuhasan.appgithubuser;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 import java.util.ArrayList;
 
@@ -26,7 +31,13 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.CardViewHolder
 
     @Override
     public void onBindViewHolder(@NonNull UserAdapter.CardViewHolder holder, int position) {
-
+        User user = listUser.get(position);
+        Glide.with(holder.itemView.getContext())
+                .load(user.getAvatar())
+                .apply(new RequestOptions().override(350, 350))
+                .into(holder.img_avatar);
+        holder.tv_name.setText(user.name);
+        holder.tv_username.setText(user.username);
     }
 
     @Override
@@ -35,9 +46,15 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.CardViewHolder
     }
 
     public static class CardViewHolder extends RecyclerView.ViewHolder {
+        ImageView img_avatar;
+        TextView tv_name;
+        TextView tv_username;
 
         public CardViewHolder(@NonNull View itemView) {
             super(itemView);
+            img_avatar = itemView.findViewById(R.id.img_avatar);
+            tv_name = itemView.findViewById(R.id.tv_name);
+            tv_username = itemView.findViewById(R.id.tv_username);
         }
     }
 }
