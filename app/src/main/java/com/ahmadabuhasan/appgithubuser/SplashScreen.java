84a19b601a7f145786884d3ff9c985f546280475
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-09-24 21:42:56 +0700

    SplashScreen

---
+++ app/src/main/java/com/ahmadabuhasan/appgithubuser/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.ahmadabuhasan.appgithubuser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                SplashScreen.this.startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                SplashScreen.this.finish();
+            }
+        }, (long) 3000);
+    }
+}
