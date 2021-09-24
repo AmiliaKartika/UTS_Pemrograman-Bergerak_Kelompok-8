0bbcbd8925422cde1d81c03c279b1257112ce4c4
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-09-24 22:48:05 +0700

    Add SearchView

--- app/src/main/java/com/ahmadabuhasan/appgithubuser/MainActivity.java
+++ app/src/main/java/com/ahmadabuhasan/appgithubuser/MainActivity.java
@@ -1,13 +1,19 @@
 package com.ahmadabuhasan.appgithubuser;
 
 import android.annotation.SuppressLint;
+import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -16,6 +22,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView rv_github;
+    UserAdapter userAdapter;
     private final ArrayList<User> list = new ArrayList<>();
     private static long back_pressed;
 
@@ -60,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void showRecyclerUser() {
         rv_github.setLayoutManager(new LinearLayoutManager(this));
-        UserAdapter userAdapter = new UserAdapter(list);
+        userAdapter = new UserAdapter(list);
         rv_github.setAdapter(userAdapter);
 
         userAdapter.setOnItemClickCallback(this::showSelectedUser);
@@ -81,5 +88,39 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText((Context) this, "Press once again to exit", Toast.LENGTH_SHORT).show();
         }
         back_pressed = System.currentTimeMillis();
+    }
+
+    @SuppressLint("ResourceType")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.searchmenu, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        final SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        MenuItem searchMenuItem = menu.findItem(R.id.action_search);
+
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName())
+        );
+        searchView.setBackgroundColor(Color.BLACK);
+        searchView.setQueryHint("Search Name...");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(final String query) {
+                userAdapter.getFilter().filter(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (userAdapter != null) {
+                    userAdapter.getFilter().filter(newText);
+                }
+                return false;
+            }
+        });
+        searchMenuItem.getIcon().setVisible(false, false);
+        return true;
     }
 }
